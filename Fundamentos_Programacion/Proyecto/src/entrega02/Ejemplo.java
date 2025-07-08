2ef1f25e85305af5e45663b73abc3c4b6d354337
Kath1206 <kcanalesg@ucenfotec.ac.cr>
2025-07-08 01:41:09 +0000

    prueba

--- Fundamentos_Programacion/Proyecto/src/entrega02/Ejemplo.java
+++ Fundamentos_Programacion/Proyecto/src/entrega02/Ejemplo.java
@@ -1,5 +1,7 @@
 package entrega02;
 
 public class Ejemplo {
-    
+    public static void main(String[] args) {
+        System.out.println("La infeccion lo cambió todo. #1 Ya casi no queda nadie con vida. Buscando refugio te adentras en una antigua casa abandonada.");
+    }
 }
